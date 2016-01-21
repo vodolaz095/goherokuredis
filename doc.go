b3a7d805bfc466655fa93fa70edfbef55b03922b
Anatolij Ostroumov <ostroumov095@gmail.com>
2016-01-21 21:58:11 +0300

    documentation improved

--- doc.go
+++ doc.go
@@ -3,9 +3,13 @@ Package goherokuredis inspects environment values for well known Redis-As-A-Serv
 connection parameters being used at Heroku. Currently - these environment variables are supported (in this order):
 
 - REDISTOGO_URL - see for details (https://devcenter.heroku.com/articles/redistogo#using-with-node-js)
+
 - OPENREDIS_URL - see for details (https://devcenter.heroku.com/articles/openredis#using-redis-from-node-js)
+
 - REDISCLOUD_URL - see for details (https://devcenter.heroku.com/articles/heroku-redis#connecting-in-node-js)
+
 - REDISGREEN_URL - see for details (https://devcenter.heroku.com/articles/redisgreen#using-redis-with-node-js)
+
 - REDIS_URL - see for details (https://devcenter.heroku.com/articles/heroku-redis#connecting-in-node-js)
 
 if all of this environment values are empty, the default connection string is being used - `redis://:@localhost:6379` - that is equal to
